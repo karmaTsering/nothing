221600b2d58c650f1c1cfc8d227bdd0f6911a731
karmaTsering <gurungkarm@mnstate.edu>
2016-05-01 14:28:04 -0500

    Delete isValidSSN.py

--- isValidSSN.py
+++
@@ -1,8 +0,0 @@
-
-def isValidSSN(tmp):
-    
-
-    if len(tmp)==11 and tmp[0:3].isdigit() and tmp[3]=='-' and tmp[4:6].isdigit() and tmp[6]=='-' and tmp[7:12].isdigit():
-        return True
-    else:
-        return False
